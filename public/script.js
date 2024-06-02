a8ad7d8dc60668cf6244baa2821066dbf3f7c85e
Vinícius Serra <125168815+vsserra@users.noreply.github.com>
2024-06-02 00:47:50 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -16,12 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const countdownElement = document.getElementById('countdown');
     const scorePlayer1 = document.getElementById('score-player1');
     const scorePlayer2 = document.getElementById('score-player2');
+    const scoreTies = document.getElementById('score-ties');
 
     let isXTurn = true;
     let gameActive = true;
     let gameState = Array(9).fill(null);
     let players = {};
-    let scores = { player1: 0, player2: 0 };
+    let scores = { player1: 0, player2: 0, ties: 0 };
     let currentPlayer = '';
     let readyPlayers = 0;
 
@@ -48,6 +49,8 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (gameState.every(cell => cell !== null)) {
             gameActive = false;
             messageElement.textContent = 'Empate!';
+            scores.ties++;
+            updateScoreboard();
         } else {
             isXTurn = !isXTurn;
             currentPlayer = isXTurn ? players.XId : players.OId;
@@ -140,11 +143,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (playerNumber === 'player1') {
             readyPlayer1Button.disabled = true;
             statusPlayer1.textContent = 'Pronto';
-            statusPlayer1.style.color = 'blue';
+            statusPlayer1.style.color = '#3498db';
         } else {
             readyPlayer2Button.disabled = true;
             statusPlayer2.textContent = 'Pronto';
-            statusPlayer2.style.color = 'blue';
+            statusPlayer2.style.color = '#3498db';
         }
 
         if (readyPlayers === 2) {
@@ -173,6 +176,7 @@ document.addEventListener('DOMContentLoaded', () => {
     socket.on('gameStarted', ({ players }) => {
         scorePlayer1.textContent = `${players['X']}: ${scores.player1} vitórias`;
         scorePlayer2.textContent = `${players['O']}: ${scores.player2} vitórias`;
+        scoreTies.textContent = `Empates: ${scores.ties}`;
         playerForm.style.display = 'none';
         scoreboard.style.display = 'block';
         restartGame();
@@ -181,6 +185,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const updateScoreboard = () => {
         scorePlayer1.textContent = `${players['X']}: ${scores.player1} vitórias`;
         scorePlayer2.textContent = `${players['O']}: ${scores.player2} vitórias`;
+        scoreTies.textContent = `Empates: ${scores.ties}`;
     };
 
     cells.forEach(cell => cell.addEventListener('click', handleCellClick));
